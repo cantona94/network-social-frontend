ee378c58f13e9962723891c7e047f574eec38591
cantona94 <vlstan@mail.ru>
2024-07-21 16:16:58 +0500

    update AuthPage

--- src/pages/auth/ui/AuthPage.tsx
+++ src/pages/auth/ui/AuthPage.tsx
@@ -1,3 +1,30 @@
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react";
+import { useState } from "react";
+
 export const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const [selected, setSelected] = useState<string>("login");
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overflow-hidden">
+            <Tabs
+              fullWidth
+              size="md"
+              selectedKey={selected}
+              onSelectionChange={key => setSelected(key as string)}
+            >
+              <Tab key="login" title="Вход">
+                login
+              </Tab>
+              <Tab key="sign-up" title="Регистрация">
+                Register
+              </Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  );
 };
