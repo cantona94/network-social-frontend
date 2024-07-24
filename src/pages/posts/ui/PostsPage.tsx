efefdd8510211f4b1e63f1a8147635de4c6fe46f
cantona94 <vlstan@mail.ru>
2024-07-24 12:23:58 +0500

    add createPost feature

--- src/pages/posts/ui/PostsPage.tsx
+++ src/pages/posts/ui/PostsPage.tsx
@@ -1,3 +1,14 @@
+import { CreatePost } from "@/features/create-post";
+import { useGetAllPostsQuery } from "@/shared/api/post-api";
+
 export const PostsPage = () => {
-  return <div>PostsPage</div>;
+  const { data } = useGetAllPostsQuery();
+
+  return (
+    <>
+      <div className="w-full flex mb-10">
+        <CreatePost />
+      </div>
+    </>
+  );
 };
