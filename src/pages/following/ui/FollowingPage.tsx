b75a941da7aa6ceb953a9ce775ac0bdce0f9c2a3
cantona94 <vlstan@mail.ru>
2024-07-27 13:02:59 +0500

    update followingPage

--- src/pages/following/ui/FollowingPage.tsx
+++ src/pages/following/ui/FollowingPage.tsx
@@ -1,3 +1,33 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Card, CardBody } from "@nextui-org/react";
+import { selectCurrent } from "@/entities/user/model/slice";
+import { User } from "@/shared/ui/user";
+
 export const FollowingPage = () => {
-  return <div>FollowingPage</div>;
+  const currentUser = useSelector(selectCurrent);
+
+  if (!currentUser) {
+    return null;
+  }
+
+  return currentUser.following.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.following.map(user => (
+        <Link to={`/user/${user.following.id}`} key={user.following.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.following.name ?? ""}
+                avatarUrl={user.following.avatarUrl ?? ""}
+                description={user.following.email ?? ""}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h2>У Вас нет ещё ни одной подписки</h2>
+  );
 };
