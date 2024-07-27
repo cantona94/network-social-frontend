543a75b2a39c1da02792601a43487ca8528a4db9
cantona94 <vlstan@mail.ru>
2024-07-27 14:02:31 +0500

    update followersPage

--- src/pages/followers/ui/FollowersPage.tsx
+++ src/pages/followers/ui/FollowersPage.tsx
@@ -1,3 +1,33 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Card, CardBody } from "@nextui-org/react";
+import { selectCurrent } from "@/entities/user/model/slice";
+import { User } from "@/shared/ui/user";
+
 export const FollowersPage = () => {
-  return <div>FollowersPage</div>;
+  const currentUser = useSelector(selectCurrent);
+
+  if (!currentUser) {
+    return null;
+  }
+
+  return currentUser.followers.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.followers.map(user => (
+        <Link to={`/user/${user.follower.id}`} key={user.follower.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.follower.name ?? ""}
+                avatarUrl={user.follower.avatarUrl ?? ""}
+                description={user.follower.email ?? ""}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h2>У вас нет подписчиков</h2>
+  );
 };
